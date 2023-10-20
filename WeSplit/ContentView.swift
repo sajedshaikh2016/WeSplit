54b8d3bde42d4ca9619dff961149693d7f530486
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-10-20 19:31:08 +0530

    Modifying program state

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,18 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
     var body: some View {
-        
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello, world!")
-                }
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
         }
-
     }
 }
 
